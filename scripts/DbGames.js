97ee6eaa83d14e18ebc6ec557abfa1f55305b0c8
emanuelhenrique-dev <emanuelhenriquefs@gmail.com>
2022-12-07 18:49:52 -0300

    create project

---
+++ scripts/DbGames.js
@@ -0,0 +1,67 @@
+const highlight_games = [
+  {
+    id: '0',
+    img: '../assets/img/God_of_war_img.png',
+    bg: '/assets/video/God_of_war.mp4',
+    art: '../assets/img/God_of_warArt.png',
+    name: 'God of War Ragnarök',
+    dev: 'Santa Monica Studio',
+    stars: '5',
+    tags: 'Ação, Rpg',
+    category: 'ARPG',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum convallis turpis, nec rutrum quam vulputate viverra. Proin ornare dolor sit amet laoreet dignissim.'
+  },
+  {
+    id: '1',
+    img: '../assets/img/Nioh_2_img.png',
+    bg: '/assets/video/Nioh_2.mp4',
+    art: '../assets/img/Nioh_2Art.png',
+    name: 'Nioh 2',
+    dev: 'Team Ninja',
+    stars: '4',
+    tags: 'Ação, Rpg',
+    category: 'ARPG',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tristique senectus et netus et malesuada fames.'
+  },
+  {
+    id: '2',
+    img: '../assets/img/Mhw_img.png',
+    bg: '/assets/video/Mhw.mp4',
+    art: '../assets/img/MhwArt.png',
+    name: 'Monster Hunter: World',
+    dev: 'Team Ninja',
+    stars: '4',
+    tags: 'Ação, Rpg, coop',
+    category: 'Aventura',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Erat imperdiet sed euismod nisi porta lorem mollis aliquam ut.'
+  },
+  {
+    id: '3',
+    img: '../assets/img/Elden_ring_img.png',
+    bg: '/assets/video/Elden_ring.mp4',
+    art: '../assets/img/Elden_ringArt.png',
+    name: 'Elden Ring',
+    dev: ' FromSoftware Inc',
+    stars: '5',
+    tags: 'Ação, Rpg, Pvp',
+    category: 'ARPG',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae congue eu.'
+  },
+  {
+    id: '4',
+    img: '../assets/img/Marvel_spider_man_img.png',
+    bg: '/assets/video/Marvel_spider_man.mp4',
+    art: '../assets/img/Marvel_spider_manArt.png',
+    name: 'Marvel’s Spider-Man',
+    dev: 'Insomniac Games',
+    stars: '3',
+    tags: 'Ação, Aventura',
+    category: 'Super-Heróis',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet risus feugiat in ante metus dictum.'
+  }
+];
---
+++ scripts/index.js
@@ -0,0 +1,110 @@
+const HlGameList = document.querySelector('.highlights ul');
+const gameShowcase = document.querySelector('.game-showcase');
+
+// HL = highlights //
+// Criar cada card highlights //
+function createCardGame(game) {
+  const Hlcard = document.createElement('li');
+
+  Hlcard.innerHTML = `
+        <p class="tag">${game.category}</p>
+        <p class="price">R$ 49,95</p>
+        <h3>${game.name}</h3>
+    `;
+  Hlcard.style.backgroundImage = 'url("' + game.img + '")';
+  Hlcard.id = game.id;
+  return Hlcard;
+}
+
+// Colocar os cards highlights dos games no ul//
+function listarGames(listaGames) {
+  for (let i = 0; i < listaGames.length; i++) {
+    const game = listaGames[i];
+
+    const cardMontado = createCardGame(game);
+
+    if (i == 0) {
+      //deixar apenas o primeiro ativo como padrão
+      cardMontado.classList.add('Active');
+    }
+
+    HlGameList.appendChild(cardMontado);
+  }
+}
+
+listarGames(highlight_games);
+
+const HlCards = document.querySelectorAll('.highlights ul li');
+
+// Deixar selecionado um jogo da lista //
+HlGameList.addEventListener('click', (e) => {
+  const gameCard = e.target;
+  let number = 0;
+  if (gameCard.tagName == 'LI') {
+    gameCard.classList.add('Active');
+    ShowGame(gameCard.id);
+
+    for (let i = 0; i < HlCards.length; i++) {
+      if (HlCards[i].id !== gameCard.id) {
+        HlCards[i].classList.remove('Active');
+      }
+    }
+  }
+});
+
+// Mudar o jogo mostrado em destaque //
+const video = document.querySelector('.video');
+function ShowGame(id) {
+  const gameSelect = highlight_games[id];
+  console.log(gameSelect.tags.split(','));
+  video.src = gameSelect.bg;
+  gameShowcase.innerHTML = `
+            <div class="tags">
+            ${createTags(gameSelect.tags)}
+            </div>
+            <div class="extra">
+              <span class="stars">
+              ${createStars(gameSelect.stars)}
+              </span>
+              <p>${gameSelect.dev}</p>
+            </div>
+            <h2>${gameSelect.name}</h2>
+            <p class="description">
+            ${gameSelect.description}
+            </p>
+            <button>Mais detalhes</button>
+            <div style= "background-image: url(${
+              gameSelect.art
+            });" class="imageArt" style:></div>
+    `;
+}
+
+// códigos responsáveis pelas tags e estrelas //
+function createStars(rate) {
+  let element = ``;
+  for (let i = 1; i <= rate; i++) {
+    element += `<i class="ph-star-fill"></i>`;
+  }
+  return element;
+}
+
+function createTags(tags) {
+  let tagsList = tags.split(',');
+  let tagCard = ``;
+  tagsList.forEach((element) => {
+    tagCard += `<p class="tag">${element}</p>`;
+  });
+  return tagCard;
+}
+
+// nav mobile //
+
+const menu = document.querySelector('.mobile-menu');
+const navList = document.querySelector('nav ul');
+
+menu.addEventListener('click', (e) => {
+  console.log('menu clicked');
+  navList.classList.toggle('active');
+  menu.classList.toggle('open');
+  document.body.classList.toggle('menu-expanded');
+});
